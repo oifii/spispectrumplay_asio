734e98a4b9c1b9c4dca8f6bc043424cdd0a079c6
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 13:16:57 -0500

    first commit

---
+++ Release/spispectrumplay_asio.bat
@@ -0,0 +1,2 @@
+rem start spispectrumplay_asio.exe testwav.wav 120 600 600 1 "E-MU ASIO" 2 3 150
+start spispectrumplay_asio.exe "D:\oifii-org\httpdocs\ha-org\had\dj-oifii\house_wav\dj-oifii_heavyhouse(Shivers-Armin van Buuren)_007.wav" 3600 600 600 1 "E-MU ASIO" 2 3 150
